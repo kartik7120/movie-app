fa25dbebb9d2a7f2a2e387925a36082baea9b7df
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-11-03 17:24:34 +0530

    Added the trending type

--- graphql/typeDefs.ts
+++ graphql/typeDefs.ts
@@ -1,10 +1,30 @@
 import { gql } from "@apollo/client";
 
-export const typeDefs = gql`
+export const typeDefs = gql` #graghql
     type User {
         name:String!
         id:ID
         password:String!
+    }
+
+    "Get the daily or weekly trending items"
+    type Trending {
+        page:Int!
+        poster_path:String
+        adult:Boolean!
+        overview:String!
+        release_date:String!
+        genre_id:[Int]!
+        id:Int!
+        original_title:String!
+        original_language:String!
+        title:String!
+        backdrop_path:String
+        popularity:Int!
+        vote_count:Int!
+        video:Boolean!
+        total_pages:Int!
+        total_results:Int!
     }
 
     type Query {
