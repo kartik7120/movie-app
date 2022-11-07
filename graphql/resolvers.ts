8c2e450239115bd7ad564f55a065ec88631cf7e6
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-11-07 13:06:15 +0530

    Removed commented lines of code

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -18,19 +18,11 @@ export const resolvers = {
             if (args.mediaType === undefined || args.mediaType === null) {
                 throw new GraphQLError("mediaType is null or undefined");
             }
-            // const query = await axios.get(`${process.env.API_URL!}trending/${args.timeWindow}/${args.mediatype}`);
-            // const result = query.data.results;
-            const query2 = await fetch(`${process.env.API_URL!}trending/${args.mediaType}/${args.timeWindow}?api_key=35aa9d224935ab595e7f1d1d7a85bf92`);
-            // https://api.themoviedb.org/3/trending/all/day?api_key=35aa9d224935ab595e7f1d1d7a85bf92
+            const query2 = await fetch(`${process.env.API_URL!}trending/
+            ${args.mediaType}/${args.timeWindow}?api_key=35aa9d224935ab595e7f1d1d7a85bf92`);
             const query2Result = await query2.json();
             const result2 = query2Result.results;
-            // console.log(result); // how to perform this console.log in the console
             return result2;
         },
     },
-    // Trending: {
-    //     genre_id(parent: any) {
-    //         return [parent.genre_id];
-    //     }
-    // }
 }
