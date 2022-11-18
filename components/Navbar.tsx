23749d21f6c69374686ba6f3d6c6f563f1087509
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-11-18 18:10:33 +0530

    Added media quries to make a responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { createStyles } from "@mantine/styles";
 import navbar from "../styles/navbar.module.css";
 import { FaImdb } from "react-icons/fa";
-import { ActionIcon, Button, Select, Divider, Drawer } from "@mantine/core";
+import { ActionIcon, Button, Select, Divider, Drawer, MediaQuery } from "@mantine/core";
 import { AiOutlineMenu } from "react-icons/ai";
 import { TextInput } from "@mantine/core";
 import { BiSearchAlt } from "react-icons/bi";
@@ -31,21 +31,43 @@ export default function Navbar(): JSX.Element {
     const { classes } = useStyles();
     const [opened, setOpened] = React.useState<boolean>(false);
 
-    return <div className={navbar.wrapper}>
+    return <> <nav className={navbar.wrapper}>
         <FaImdb size={50} color="#F08C00" />
         <Button leftIcon={<AiOutlineMenu />} variant="filled" className={navbar.navMenu}>Menu</Button>
         <TextInput placeholder="Search" icon={<BiSearchAlt />} style={{ width: "60%" }} rightSectionWidth={100}
             rightSection={<Select placeholder="Select" data={[
                 { value: "all", label: "All" }
             ]} />} />
-        <Drawer opened={opened} onClose={() => setOpened(false)} title="Menu" padding="xl" size="xl">
+        <Drawer opened={opened} onClose={() => setOpened(false)} title="Menu" padding="xl" size="md">
             <div>Menu Content</div>
+            <div>Watch list</div>
         </Drawer>
         <ActionIcon className={navbar.drawer} onClick={() => setOpened(true)}>
             <FiMenu />
         </ActionIcon>
-        <Divider orientation="vertical" size="md" m={0} />
-        <Button leftIcon={<BsFillBookmarkPlusFill />} variant="filled">Watchlist</Button>
-        <Button leftIcon={<Avatar src={null} radius="md" />} variant="subtle">Profile</Button>
-    </div>
+        <Divider orientation="vertical" size="md" m={0} className={navbar.divider} />
+        <Button leftIcon={<BsFillBookmarkPlusFill />} variant="filled" className={navbar.watchlist}>Watchlist</Button>
+        <Button leftIcon={<Avatar src={null} radius="md" />} className={navbar.profileBtn} variant="subtle">Profile</Button>
+    </nav>
+        <nav className={navbar.wrapper2}>
+            <div className={navbar.wrapper2Div}>
+                <FaImdb size={50} color="#F08C00" />
+                <Drawer opened={opened} onClose={() => setOpened(false)} title="Menu" padding="xl" size="md">
+                    <div>Menu Content</div>
+                    <div>Watch list</div>
+                </Drawer>
+                <ActionIcon className={navbar.drawer} onClick={() => setOpened(true)}>
+                    <FiMenu />
+                </ActionIcon>
+            </div>
+            <div className={navbar.wrapper2Div}>
+                <ActionIcon>
+                    <BiSearchAlt />
+                </ActionIcon>
+                <ActionIcon>
+                    <Avatar src={null} radius="md" />
+                </ActionIcon>
+            </div>
+        </nav>
+    </>
 }
