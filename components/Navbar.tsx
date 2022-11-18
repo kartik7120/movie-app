1aabeeccd7295d59553aea812b334137be4e7412
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-11-18 15:16:07 +0530

    Added a navbar to the website

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,18 +29,18 @@ export default function Navbar(): JSX.Element {
     const { classes } = useStyles();
 
     return <div className={navbar.wrapper}>
-        <div className={classes.wrapper}>
+        {/* <div className={classes.wrapper}> */}
             <FaImdb size={50} color="#F08C00" />
             <Button leftIcon={<AiOutlineMenu />} variant="filled">Menu</Button>
-            <TextInput placeholder="Search" icon={<BiSearchAlt />} style={{ width: "50em" }} rightSectionWidth={100}
+            <TextInput placeholder="Search" icon={<BiSearchAlt />} style={{ width: "60%" }} rightSectionWidth={100}
                 rightSection={<Select placeholder="Select" data={[
                     { value: "all", label: "All" }
                 ]} />} />
-        </div>
-        <div className={classes.wrapper2}>
+        {/* </div> */}
+        {/* <div className={classes.wrapper2}> */}
             <Divider orientation="vertical" size="md" m={0} />
             <Button leftIcon={<BsFillBookmarkPlusFill />} variant="filled">Watchlist</Button>
             <Button leftIcon={<Avatar src={null} radius="md" />} variant="subtle">Profile</Button>
-        </div>
+        {/* </div> */}
     </div>
 }
