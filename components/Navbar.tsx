a6508ce9db477513f4880d8e3f297fd0bfbbc20d
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-11-24 16:46:27 +0530

    Added the hotkey hook on the text input and handleSubmit event on pressing enter textinput

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { createStyles } from "@mantine/styles";
 import navbar from "../styles/navbar.module.css";
 import { FaImdb } from "react-icons/fa";
-import { ActionIcon, Button, Select, Divider, Drawer, MediaQuery } from "@mantine/core";
+import { ActionIcon, Button, Select, Divider, Drawer } from "@mantine/core";
 import { AiOutlineMenu } from "react-icons/ai";
 import { TextInput } from "@mantine/core";
 import { BiSearchAlt } from "react-icons/bi";
@@ -9,6 +9,8 @@ import { BsFillBookmarkPlusFill } from "react-icons/bs";
 import { Avatar, Modal } from "@mantine/core";
 import React from "react";
 import { FiMenu } from "react-icons/fi";
+import { getHotkeyHandler } from '@mantine/hooks';
+import { useRouter } from "next/router";
 
 const useStyles = createStyles((theme, params, getRef) => ({
     wrapper: {
@@ -28,21 +30,29 @@ const useStyles = createStyles((theme, params, getRef) => ({
 }))
 
 export default function Navbar(): JSX.Element {
+    const router = useRouter();
     const { classes } = useStyles();
     const [opened, setOpened] = React.useState<boolean>(false);
     const [modelOpened, setModelOpened] = React.useState(false);
 
+    function handleSubmit(e: React.KeyboardEvent<HTMLElement> | KeyboardEvent) {
+        router.push("/search");
+    }
 
     return <> <nav className={navbar.wrapper}>
-        <Modal opened={modelOpened} fullScreen={true} transition="slide-down" exitTransitionDuration={100} onClose={() => setModelOpened(false)} title="Menu Model" >
+        <Modal opened={modelOpened} fullScreen={true} transition="slide-down" exitTransitionDuration={100}
+            onClose={() => setModelOpened(false)} title="Menu Model" >
             <h1>Look at me I am a web developer</h1>
         </Modal>
-        <FaImdb size={50} color="#F08C00" />
-        <Button leftIcon={<AiOutlineMenu />} variant="filled" onClick={() => setModelOpened(true)} className={navbar.navMenu}>Menu</Button>
-        <TextInput placeholder="Search" icon={<BiSearchAlt />} style={{ width: "60%" }} rightSectionWidth={100}
+        <FaImdb size={50} color="#F08C00" className={navbar.cursor} onClick={() => router.push("/")} />
+        <Button leftIcon={<AiOutlineMenu />} variant="filled" onClick={() => setModelOpened(true)}
+            className={navbar.navMenu}>Menu</Button>
+        <TextInput type="text" placeholder="Search" icon={<BiSearchAlt />} style={{ width: "60%" }} rightSectionWidth={100}
             rightSection={<Select placeholder="Select" data={[
                 { value: "all", label: "All" }
-            ]} />} />
+            ]} />} onKeyDown={getHotkeyHandler([
+                ['enter', handleSubmit]
+            ])} />
         <Drawer opened={opened} onClose={() => setOpened(false)} title="Menu" padding="xl" size="md">
             <div>Menu Content</div>
             <div>Watch list</div>
