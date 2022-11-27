5eafd197544fe93218ab01322e29499e3b6dbad3
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-11-27 11:54:07 +0530

    Added graphql query and some components for upper part of movie page

--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -4,31 +4,29 @@ import client from "../../apollo-client";
 import styles from "../../styles/movie.module.css";
 import React from "react";
 import ImageCard from "../../components/ImageCard";
-import { Title } from "@mantine/core";
+import { Text, Title } from "@mantine/core";
 import Head from "next/head";
+import { runTimeConversion, covertDataFormat } from "../../lib/util";
 
-type data = {
-    name: string
-}
 const MOVIE_DETAILS = gql`
 query GetMovieDetails($getMovieDetailsId: ID!) {
   getMovieDetails(id: $getMovieDetailsId) {
-    # backdrop_path
-    # budget
+    backdrop_path
+    budget
     title
-    # release_date
+    release_date
     poster_path
-    # runtime
-    # revenue
-    # overview
-    # genres {
-    #   name
-    # }
-    # original_language
-    # homepage
-    # status
-    # vote_average
-    # tagline
+    runtime
+    revenue
+    overview
+    genres {
+      name
+    }
+    original_language
+    homepage
+    status
+    vote_average
+    tagline
   }
 }
 `
@@ -43,6 +41,11 @@ export default function Media({ data, id }: { data: any, id: number }) {
             <div>
                 <ImageCard imgUrl={data.poster_path} title={data.title} />
                 <Title order={1} size="h1">{data.title}</Title>
+                <Text variant="text" >{data.genres.map((ele: { name: string }) => ele.name).join(",")}</Text>
+                <Text variant="text" component="p">{covertDataFormat(data.release_date)}</Text>
+                <Text variant="text" component="p">{data.overview}</Text>
+                <Text variant="text" component="p">{data.tagline}</Text>
+                <Text variant="text" component="span">{runTimeConversion(data.runtime)}</Text>
             </div>
             <div>Grid div</div>
         </div>
