edb0e3e4fae49748237ba8ace6d071d127a20e4d
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-12-01 17:41:42 +0530

    added component that will render a poster

---
+++ components/Posters.tsx
@@ -0,0 +1,48 @@
+import { gql, useQuery } from "@apollo/client";
+import { Button, ScrollArea } from "@mantine/core";
+import Image from "next/image";
+import styles from "../styles/movie.module.css";
+import { AiOutlineArrowRight } from "react-icons/ai";
+
+const IMAGES = gql`
+    query GetImageMedia($getImageMediaId: ID!, $sourceMedia: SourceMedia!, $first: Int) {
+  getImageMedia(id: $getImageMediaId, sourceMedia: $sourceMedia, first: $first) {
+    posters {
+      file_path
+    }
+  }
+}
+`
+
+interface Props {
+    id: number,
+    sourceMedia: "MOVIE" | "TV",
+    first?: number
+}
+
+export default function Posters(props: Props): JSX.Element {
+
+    const { loading, error, data } = useQuery(IMAGES, {
+        variables: {
+            getImageMediaId: props.id,
+            sourceMedia: props.sourceMedia,
+            first: 7
+        },
+        onCompleted(data) {
+            console.log(data)
+        },
+    })
+
+    if (loading)
+        return <p>Loading...</p>
+
+    return <ScrollArea style={{ width: 1000 }}>
+        <div className={styles.videoWrapper}>
+            {data && data.getImageMedia.posters.map((img: any, index: number) => {
+                return <Image key={Math.random() * index * 47} src={`https://image.tmdb.org/t/p/w200${img.file_path}?api_key=${process.env.API_KEY}`}
+                    width={200} height={300} alt="Poster" />
+            })}
+            <Button type="button" rightIcon={<AiOutlineArrowRight />}>View More</Button>
+        </div>
+    </ScrollArea>
+}
