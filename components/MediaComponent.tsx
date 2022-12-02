a4c22290517d51bae4f3e6704c0293e96a816fa9
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-12-02 09:19:40 +0530

    Removed the popular tab and added horizontal wrapper class

--- components/MediaComponent.tsx
+++ components/MediaComponent.tsx
@@ -11,15 +11,13 @@ interface Props {
 
 export default function MediaComponent(props: Props): JSX.Element {
     return (
-        <Tabs defaultValue="most popular">
+        <Tabs defaultValue="backdrops">
             <Tabs.List>
-                <Tabs.Tab value="most popular">Most Popular</Tabs.Tab>
                 <Tabs.Tab value="videos">Videos</Tabs.Tab>
                 <Tabs.Tab value="backdrops">Backdrops</Tabs.Tab>
                 <Tabs.Tab value="posters">Posters</Tabs.Tab>
             </Tabs.List>
 
-            <Tabs.Panel value="most popular">Most Popular Panel</Tabs.Panel>
             <Tabs.Panel value="videos"><Videos id={props.id} sourceMedia={props.sourceMedia} first={props.first} /></Tabs.Panel>
             <Tabs.Panel value="backdrops"><Backdrops id={props.id} sourceMedia={props.sourceMedia} first={props.first} /></Tabs.Panel>
             <Tabs.Panel value="posters"><Posters id={props.id} sourceMedia={props.sourceMedia} first={props.first} /></Tabs.Panel>
