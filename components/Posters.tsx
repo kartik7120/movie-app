51c7d758a06021fc58095ed9f55b9b3feba459fd
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-12-07 13:54:20 +0530

    Added Poster component

--- components/Posters.tsx
+++ components/Posters.tsx
@@ -3,6 +3,7 @@ import { Button, ScrollArea } from "@mantine/core";
 import Image from "next/image";
 import styles from "../styles/movie.module.css";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import { useRouter } from "next/router";
 
 const IMAGES = gql`
     query GetImageMedia($getImageMediaId: ID!, $sourceMedia: SourceMedia!, $first: Int) {
@@ -39,7 +40,7 @@ export default function Posters(props: Props): JSX.Element {
                 return <Image key={Math.random() * index * 47} src={`https://image.tmdb.org/t/p/w200${img.file_path}?api_key=${process.env.API_KEY}`}
                     width={200} height={300} alt="Poster" />
             })}
-            <Button type="button" rightIcon={<AiOutlineArrowRight />}>View More</Button>
+            <Button type="button" component="a" href={`/movie/${props.id}/images?type=posters`} rightIcon={<AiOutlineArrowRight />}>View More</Button>
         </div>
     </ScrollArea>
 }
