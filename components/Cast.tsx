462069029ae9cf49a1523369b6ee6cbbde1f0649
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-12-12 16:22:58 +0530

    Added Link tag to navigate to people page

--- components/Cast.tsx
+++ components/Cast.tsx
@@ -4,6 +4,7 @@ import { gql } from "@apollo/client";
 import ImageCard from "./ImageCard";
 import { Button, Text, Title } from "@mantine/core";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import Link from "next/link";
 
 const CAST = gql`
 query GetCast($getCastId: ID!, $mediaType: SourceMedia!, $first: Int) {
@@ -43,11 +44,13 @@ export default function Cast(props: Props): JSX.Element {
   return <>
     <Carousel slideSize="10%" align="start" height={300} slideGap="sm" dragFree>
       {data && data.getCast.cast.map((ele: any, index: number) => {
-        return <Carousel.Slide key={Math.random() * index * 41}>
-          <ImageCard imgUrl={ele.profile_path} width={150} height={200} title={`${ele.name} image`} />
-          <Text fw="bold" size="md" variant="text" >{ele.name}</Text>
-          <Text size="sm" variant="text" >{ele.character}</Text>
-        </Carousel.Slide>
+        return <Link href={`/people/${ele.id}`} key={Math.random() * index * 41}>
+          <Carousel.Slide >
+            <ImageCard imgUrl={ele.profile_path} width={150} height={200} title={`${ele.name} image`} />
+            <Text fw="bold" size="md" variant="text" >{ele.name}</Text>
+            <Text size="sm" variant="text" >{ele.character}</Text>
+          </Carousel.Slide>
+        </Link>
       })}
       <Carousel.Slide style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
         <Button type="button" size="sm" component="a" href="#" rightIcon={<AiOutlineArrowRight />}>
