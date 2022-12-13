e69aa86a973f7c6d4606550cfeb71024c1418918
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-12-13 16:42:05 +0530

    Added getServerSideProps for people index page

--- pages/people/[id]/index.tsx
+++ pages/people/[id]/index.tsx
@@ -1,3 +1,60 @@
-export default function People(): JSX.Element {
-    return <h1>I will render people&apos;s information</h1>
+import { gql } from "@apollo/client";
+import { GetServerSideProps } from "next"
+import client from "../../../apollo-client";
+import { PeopleDetails } from "../../../schemaTypes";
+const PEOPLE_DETAILS = gql`
+    query PeopleDetails($peopleDetailsId: ID!) {
+    peopleDetails(id: $peopleDetailsId) {
+    biography
+    also_known_as
+    birthday
+    deathday
+    gender
+    imdb_id
+    name
+    place_of_birth
+    profile_path
+    homepage
+  }
+}
+`
+
+interface Props {
+    people: PeopleDetails
+}
+
+export default function People(props: PeopleDetails): JSX.Element {
+
+    return <>
+        <h1>I will render people&apos;s information</h1>
+    </>
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { params, query } = context;
+
+    if (params && (params.id === undefined || params.id === null)) {
+        return {
+            notFound: true
+        }
+    }
+
+    const { data, error } = await client.query({
+        query: PEOPLE_DETAILS,
+        variables: {
+            peopleDetailsId: params && params.id ? params.id : null
+        }
+    })
+
+    if (error) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            people: data.peopleDetails
+        }
+    }
 }
