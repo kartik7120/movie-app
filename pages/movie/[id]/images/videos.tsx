7727bb6cef703f4599e83aa286821e17e30e96ad
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-12-21 12:11:54 +0530

    Added styles to make video player work

--- pages/movie/[id]/images/videos.tsx
+++ pages/movie/[id]/images/videos.tsx
@@ -7,12 +7,13 @@ import { gql, useLazyQuery, useQuery } from "@apollo/client";
 import { AiOutlineArrowRight, AiOutlinePlayCircle } from "react-icons/ai";
 import ReactPlayer from "react-player/youtube";
 import styles from "../../../../styles/movie.module.css";
-import { Button, Card, ScrollArea, Text } from "@mantine/core";
+import { ActionIcon, Portal, Card, Modal, ScrollArea, Text } from "@mantine/core";
 import Link from "next/link";
 import { GetServerSideProps } from "next";
 import client from "../../../../apollo-client";
 import React from "react";
 import videoStyles from "../../../../styles/videos.module.css";
+import { BsFillPlayCircleFill } from "react-icons/bs";
 
 const VIDEOS = gql`
     query GetVideoMedia(
@@ -64,24 +65,50 @@ interface Props {
 
 export default function Videos(props: Props) {
     const matches = useMediaQuery('(max-width:530px)');
+    const [opened, setOpened] = React.useState(false);
+    const [url, setUrl] = React.useState<string | undefined>(undefined);
+
     return <>
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
             <title>{props.title} - Videos</title>
         </Head>
+
         <MoreTitle id={props.id} title={`${props.title || "Movie Title"}`} />
         <div className={styles.wrapper}>
             <div className={styles.wrapper3}>
                 <LeftOptions type="videos" id={props.id} title="Videos" videoList={props.typeMedia} />
             </div>
             <div className={styles.wrapper2}>
+                <span id="playerModel"></span>
+                <Modal size="auto" closeOnClickOutside={false} centered opened={opened} onClose={() => {
+                    setOpened(false);
+                    setUrl(undefined);
+                }}>
+                    <ReactPlayer playing stopOnUnmount width={1000} height={500}
+                        controls={true} url={url} />
+                </Modal>
                 {props.data.map((ele: { key: string, value: any[] }, index: number) => {
                     return <div key={Math.random() * index * 7} className={videoStyles.wrapper3}>
                         {ele.value.map((ele, index: number) => {
                             return <Card key={Math.random() * index * 69} w={1000} m={10} radius="lg">
                                 <div className={videoStyles.wrapper}>
-                                    <ReactPlayer light={true} width={400} height={200} url={`https://www.youtube.com/watch?v=${ele.key}`} />
+                                    <div>
+                                        <div className={videoStyles.playerWrapper}>
+                                            <ReactPlayer light={true} width={400} height={200} previewTabIndex={1}
+                                                controls={false} url={`https://www.youtube.com/watch?v=${ele.key}`} />
+                                            <div className={videoStyles.playerOverley}>
+                                                <ActionIcon size="xl"
+                                                    opacity={1} onClick={() => {
+                                                        setUrl(`https://www.youtube.com/watch?v=${ele.key}`);
+                                                        setOpened(true);
+                                                    }}>
+                                                    <BsFillPlayCircleFill color="black" size={50} />
+                                                </ActionIcon>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <div className={videoStyles.wrapper2}>
                                         <Text variant="text" fw="bold" size="lg">{ele.name}</Text>
                                         <Text variant="text" fw="bold" size="lg">{ele.site}</Text>
