9940de506c63cbd05a379ca7f37b76f789db203c
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-12-22 17:44:03 +0530

    Some changes in the image component to make it render placeholder if img url is null

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -2,7 +2,7 @@ import { Card, Image } from "@mantine/core";
 
 interface Props {
     imgUrl: string,
-    title?: string | string[],
+    title?: string | string[] | null,
     width: number,
     height: number
 }
@@ -11,7 +11,7 @@ export default function ImageCard(props: Props): JSX.Element {
     return (
         <Card shadow="sm" p="lg" withBorder radius="md">
             <Card.Section>
-                <Image src={`https://image.tmdb.org/t/p/w300${props.imgUrl}`} alt={`${props.title || "Media Image"}`}
+                <Image src={props.imgUrl ? `https://image.tmdb.org/t/p/w300${props.imgUrl}` : null} alt={`${props.title || "Media Image"}`}
                     withPlaceholder width={props.width} height={props.height} />
             </Card.Section>
         </Card>
