65030507e4aec0373a94643ecf8a37a848466d3b
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-12-23 11:50:37 +0530

    Added conditional rendering for social icons

--- components/Social.tsx
+++ components/Social.tsx
@@ -38,25 +38,34 @@ export default function Social(props: Props): JSX.Element {
     }
 
     return <Group position="left" pl={30}>
-        <Tooltip label="Visit Twitter" color="blue" withArrow>
+
+        {data.getExternalIDs.twitter_id && <Tooltip label="Visit Twitter" color="blue" withArrow>
             <ActionIcon size="xl" component="a" target="_blank" href={`https://twitter.com/${data.getExternalIDs.twitter_id}`}>
                 <AiOutlineTwitter size={30} />
             </ActionIcon>
         </Tooltip>
-        <Tooltip label="Visit Facebook" color="blue" withArrow>
-            <ActionIcon size="xl" component="a" target="_blank" href={`https://www.facebook.com/${data.getExternalIDs.facebook_id}`}>
-                <AiFillFacebook size={30} />
-            </ActionIcon>
-        </Tooltip>
-        <Tooltip label="Visit Instagram" color="blue" withArrow>
-            <ActionIcon size="xl" component="a" target="_blank" href={`https://www.facebook.com/${data.getExternalIDs.instagram_id}`}>
-                <AiFillInstagram size={30} />
-            </ActionIcon>
-        </Tooltip>
-        <Tooltip label="Visit Homepage" color="blue" withArrow>
+        }
+
+        {data.getExternalIDs.facebook_id &&
+            <Tooltip label="Visit Facebook" color="blue" withArrow>
+                <ActionIcon size="xl" component="a" target="_blank" href={`https://www.facebook.com/${data.getExternalIDs.facebook_id}`}>
+                    <AiFillFacebook size={30} />
+                </ActionIcon>
+            </Tooltip>
+        }
+
+        {data.getExternalIDs.instagram_id &&
+            <Tooltip label="Visit Instagram" color="blue" withArrow>
+                <ActionIcon size="xl" component="a" target="_blank" href={`https://www.facebook.com/${data.getExternalIDs.instagram_id}`}>
+                    <AiFillInstagram size={30} />
+                </ActionIcon>
+            </Tooltip>
+        }
+
+        {props.homepage && <Tooltip label="Visit Homepage" color="blue" withArrow>
             <ActionIcon size="xl" component="a" target="_blank" href={`${props.homepage}`}>
                 <BiLink size={30} />
             </ActionIcon>
-        </Tooltip>
+        </Tooltip>}
     </Group>
 }
