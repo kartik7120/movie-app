4ad6a4b1a3df165cabd290bbf46186104ca828fc
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-01-10 12:58:11 +0530

    Added tests for month function (unit test)

--- tests/util.test.ts
+++ tests/util.test.ts
@@ -1,5 +1,22 @@
-import { it, describe } from "vitest"
+import { it, describe, expect } from "vitest"
+import { GetDate, monthArr } from "../lib/util"
 
 describe("covertDataFormat()", () => {
     it.todo("should return a valid string if a date string is specified")
+})
+
+describe("GetDate()", () => {
+    it("should return null when the date specified is null", () => {
+        const date = null;
+        const result = GetDate(date);
+        expect(result).toBeNull();
+    })
+
+    it("should return a valid string when a date string is given as input in a form of MM-DD-YYYY", () => {
+        const date = "2014-10-21";
+        const result = GetDate(date);
+        const d = new Date(date);
+        const expectedValue = `${monthArr[d.getMonth()]} ${d.getDay()},${d.getFullYear()}`;
+        expect(result).toEqual(`October 21,2014`)
+    })
 })
