80e99abe3fccc75431f05e09cb12a8fd93e3b584
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-01-12 19:17:27 +0530

    Added conditional rendering for profile button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,28 +1,60 @@
 import navbar from "../styles/navbar.module.css";
 import { FaImdb } from "react-icons/fa";
-import { ActionIcon, Button, Select, Divider, Drawer } from "@mantine/core";
+import { ActionIcon, Button, Select, Divider, Drawer, Menu } from "@mantine/core";
 import { AiOutlineMenu } from "react-icons/ai";
 import { TextInput } from "@mantine/core";
 import { BiSearchAlt } from "react-icons/bi";
 import { BsFillBookmarkPlusFill, BsMoon, BsSun } from "react-icons/bs";
 import { Avatar, Modal } from "@mantine/core";
-import React from "react";
+import React, { useEffect } from "react";
 import { FiMenu } from "react-icons/fi";
 import { getHotkeyHandler } from '@mantine/hooks';
 import { useRouter } from "next/router";
 import { useMantineColorScheme } from "@mantine/core";
+import { auth } from "../firebase";
+import { signOut, onAuthStateChanged } from "firebase/auth";
 
 export default function Navbar(): JSX.Element {
+    const user = auth.currentUser;
+    const [signedIn, setSignedIn] = React.useState(false);
     const { colorScheme, toggleColorScheme } = useMantineColorScheme();
     const dark = colorScheme === 'dark';
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            if (signedIn === false) {
+                setSignedIn(true);
+            }
+        }
+        else {
+            if (signedIn === true) {
+                setSignedIn(false);
+            }
+        }
+    })
 
     const router = useRouter();
     const [opened, setOpened] = React.useState<boolean>(false);
     const [modelOpened, setModelOpened] = React.useState(false);
     const [value, setValue] = React.useState("");
 
+    useEffect(() => {
+        if (user) {
+            setSignedIn(true);
+        }
+    }, [user])
+
     function handleSubmit(e: React.KeyboardEvent<HTMLElement> | KeyboardEvent) {
         router.push(`/search?query=${value}`);
+    }
+
+    async function handleSignOut() {
+        await signOut(auth).then(() => {
+            console.log('user logged out');
+            setSignedIn(false);
+        }).catch((error) => {
+            console.log(`error occured while logging out the user = ${error}`)
+        })
     }
 
     return <> <nav className={navbar.wrapper}>
@@ -50,8 +82,17 @@ export default function Navbar(): JSX.Element {
         </ActionIcon>
         <Divider orientation="vertical" size="md" m={0} className={navbar.divider} />
         <Button leftIcon={<BsFillBookmarkPlusFill />} variant="filled" className={navbar.watchlist}>Watchlist</Button>
-        <Button leftIcon={<Avatar src={null} radius="md" />} className={navbar.profileBtn} variant="subtle">Profile</Button>
-        <Button className={navbar.profileBtn} onClick={() => router.push(`/signIn`)} variant="subtle">Sign In</Button>
+
+        {signedIn ? <Menu shadow="md" width={200}>
+            <Menu.Target>
+                <Button leftIcon={<Avatar src={null} radius="md" />} className={navbar.profileBtn} variant="subtle">Profile</Button>
+            </Menu.Target>
+            <Menu.Dropdown>
+                <Menu.Item onClick={handleSignOut}>
+                    Sign out
+                </Menu.Item>
+            </Menu.Dropdown>
+        </Menu> : <Button className={navbar.profileBtn} onClick={() => router.push(`/signIn`)} variant="subtle">Sign In</Button>}
         <ActionIcon
             variant="outline"
             color={dark ? 'yellow' : 'blue'}
