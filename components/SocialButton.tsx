f5d381a0f51bfd8be0ffa1d48fc2a902b78eff50
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-01-12 19:16:22 +0530

    Added a signIn button using Google

---
+++ components/SocialButton.tsx
@@ -0,0 +1,31 @@
+import { auth } from "../firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { FcGoogle } from "react-icons/fc";
+import { Button } from "@mantine/core";
+import React from "react";
+
+
+export default function GoogleButton() {
+    const provider = new GoogleAuthProvider();
+
+    async function handleSignin() {
+        signInWithPopup(auth, provider).then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential!.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+        }).catch((error: any) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+        })
+    }
+
+    return <>
+        <Button variant="default" leftIcon={<FcGoogle />} onClick={handleSignin}>Google</Button>
+    </>
+
+}
