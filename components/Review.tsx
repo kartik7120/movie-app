633789b2790b84c19183e210916293674916b085
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-01-14 11:42:50 +0530

    Added imageCard and Title for review component

--- components/Review.tsx
+++ components/Review.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import { Button, Checkbox, Drawer, Rating, Text, Textarea, TextInput, Title } from "@mantine/core";
+import ImageCard from "./ImageCard";
+import styles from "../styles/review.module.css";
 
-export default function Review() {
+interface Props {
+    title: string,
+    imgUrl: string
+}
+
+export default function Review(props: Props) {
     const [opened, setOpened] = React.useState(false);
 
     return <>
@@ -9,17 +16,23 @@ export default function Review() {
             transition="rotate-left"
             transitionDuration={250}
             transitionTimingFunction="ease"
-            onClose={() => setOpened(false)} title="Review" padding="xl" size="xl">
-            <Rating count={10} defaultValue={1} />
+            onClose={() => setOpened(false)} title={<Title order={2} size="h1">Review</Title>} padding="xl" size="xl">
+            <div className={styles.wrapper}>
+                <ImageCard height={160} width={100} w="w200" imgUrl={props.imgUrl} />
+                <Title order={3} pl={15} size="h2">{props.title}</Title>
+            </div>
+            <Text size="lg">Your rating</Text>
+            <Rating count={10} defaultValue={1} m={10} ml={0} />
             <Text size="lg">Your Review</Text>
-            <TextInput placeholder="Write a headline for your review here" withAsterisk required label="Title" />
-            <Textarea placeholder="Write your review here" label="Review"
+            <TextInput placeholder="Write a headline for your review here" mt={5} withAsterisk required label="Title" />
+            <Textarea maxRows={4} placeholder="Write your review here" label="Review" mt={20}
                 autosize minRows={4} withAsterisk required maxLength={600} />
             <Checkbox
                 label="Does this review contain spolier"
                 size="md"
+                mt={20}
             />
-            <Button type="submit">Submit</Button>
+            <Button type="submit" mt={20} fullWidth>Submit</Button>
         </Drawer>
         <Button onClick={() => setOpened(true)}>Add Review</Button>
     </>
