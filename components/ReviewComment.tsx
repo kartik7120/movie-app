e844f79f555cc6c2a38a88dadadbe0a477f072ec
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-01-16 12:54:33 +0530

    Added review start and review reating

--- components/ReviewComment.tsx
+++ components/ReviewComment.tsx
@@ -1,6 +1,7 @@
 import { ActionIcon, Badge, Group, Spoiler, Text } from "@mantine/core";
 import { BiDislike, BiLike } from "react-icons/bi";
 import { createStyles } from "@mantine/core";
+import { AiFillStar } from "react-icons/ai";
 
 const useStyles = createStyles((theme, params, getRef) => ({
     wrapper: {
@@ -13,7 +14,8 @@ interface Props {
     review: string,
     upvotes?: number,
     downvotes?: number,
-    spolier: boolean
+    spolier: boolean,
+    rating: number
 }
 
 export default function ReviewComment(props: Props) {
@@ -21,6 +23,9 @@ export default function ReviewComment(props: Props) {
     const styles = useStyles();
 
     return <div className={styles.classes.wrapper}>
+        <Group position="left">
+            <AiFillStar color="#c39400" size={20} /> {props.rating} / 10
+        </Group>
         <Text size="xl" fw="bold">{props.title}</Text>
         {props.spolier ? <Badge variant="filled" color="red">Spolier</Badge> : ""}
         <Spoiler maxHeight={props.spolier ? 0 : 120} showLabel="Show more" hideLabel="Hide">
