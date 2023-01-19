224b552657dcee3c6d2dae768efa9c81a3e7f269
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-01-19 16:30:28 +0530

    Added favlist

--- pages/watchlist.tsx
+++ pages/watchlist.tsx
@@ -10,23 +10,31 @@ export default function WatchList(): JSX.Element {
     const auth = getAuth();
     const user = auth.currentUser;
     const [watchList, setWatchList] = useState<any[] | null>(null);
+    const [favList, setFavList] = useState<any[] | null>(null);
     useEffect(() => {
         if (user) {
             const colRef = collection(db, "users", user?.uid, "watchlist");
-            onSnapshot(colRef, (snapshot) => {
+            const unsubscribe = onSnapshot(colRef, (snapshot) => {
                 const arr = new Array(0);
                 snapshot.forEach((ele) => {
                     arr.push(ele.data());
                 })
                 setWatchList(arr);
-                snapshot.docChanges().forEach((ele) => {
-                    if (ele.type === "added") {
-                        if (watchList === null) {
-                        }
-                    }
-                })
             })
 
+            const colRef2 = collection(db, "users", user.uid, "favlist");
+            const unsubscribe2 = onSnapshot(colRef2, (snapshot) => {
+                const arr = new Array(0);
+                snapshot.forEach((ele) => {
+                    arr.push(ele.data());
+                })
+                setFavList(arr);
+            })
+
+            return () => {
+                unsubscribe();
+                unsubscribe2();
+            }
         }
 
     }, [])
@@ -48,7 +56,12 @@ export default function WatchList(): JSX.Element {
                         return <WatchListItem id={watch.id} mediaType={watch.mediaType.toUpperCase()} key={watch.id} />
                     })}
                 </Tabs.Panel>
-                <Tabs.Panel value="favourite">Favourite list</Tabs.Panel>
+                <Tabs.Panel value="favourite">
+                    {favList && favList.map((watch) => {
+                        console.log(`watch list item  = ${JSON.stringify(watch)}`)
+                        return <WatchListItem id={watch.id} mediaType={watch.mediaType.toUpperCase()} key={watch.id} />
+                    })}
+                </Tabs.Panel>
             </Tabs>
         </Container>
     </>
