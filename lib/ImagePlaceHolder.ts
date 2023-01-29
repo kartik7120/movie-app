a8ad3833e720fbe1986ba90bfcd93e96bd43258f
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-01-29 13:01:50 +0530

    Removed image placeholder file

--- lib/ImagePlaceHolder.ts
+++
@@ -1,9 +0,0 @@
-import { getPlaiceholder } from "plaiceholder";
-
-export default async function ImagePlaceHolder(uri: string): Promise<{
-    blurDataURL: string, img: any
-}> {
-    const { base64, img } = await getPlaiceholder(uri);
-
-    return { blurDataURL: base64, img }
-}
